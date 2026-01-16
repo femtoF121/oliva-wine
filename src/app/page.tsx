c26fdab4f58942fcb5ffab023beab2c21f8b52d2
femtoF <yukhym.kashchenko@gmail.com>
2026-01-16 21:51:35 +0200

    feat: implement product fetching functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-import { Button } from "@/components/ui/button";
+import { getProductById, getProducts } from "@/lib/api/products";
 
-export default function Home() {
+export default async function Home() {
+  const { data } = await getProducts();
+  const { data: dataById } = await getProductById("wine-2");
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div>
-        <Button>Click me</Button>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-100 gap-4">
+      {data.map((wine) => (
+        <div key={wine.id} className="border p-4 rounded-lg bg-background ">
+          {wine.name}
+        </div>
+      ))}
+      <div className="border p-4 rounded-lg bg-background ">
+        <h2>Product by ID:</h2>
+        {dataById.name}
       </div>
     </div>
   );
