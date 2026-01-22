286d41794bc370e911990ccd643d5a35e652007f
femtoF121 <yukhym.kashchenko@gmail.com>
2026-01-23 01:52:40 +0200

    feat: created footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${lora.variable} ${inter.variable} antialiased min-h-screen relative`}
       >
         <Header />
-        <main className="mt-20">{children}</main>
+        <main className="pt-20">{children}</main>
         <Footer />
       </body>
     </html>
