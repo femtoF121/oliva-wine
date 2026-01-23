1608a8ad25bec734f21bd96188f5a43d5703be97
femtoF121 <yukhym.kashchenko@gmail.com>
2026-01-23 02:14:48 +0200

    feat: updated header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,11 +1,23 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { FiHome, FiShoppingCart } from "react-icons/fi";
 import { Button } from "../ui/button";
 
-export function Header() {
+export function Header({
+  variant = "fill",
+}: {
+  variant?: "gradient" | "fill";
+}) {
   return (
-    <header className="fixed top-0 w-full font-lora">
+    <header
+      className={cn(
+        "fixed top-0 w-full font-lora text-[#211F1A]",
+        variant === "gradient"
+          ? "bg-[radial-gradient(95.41%_95.41%_at_50%_50%,#E7B10A,#FDEED1)]"
+          : "bg-[#FDEED1]",
+      )}
+    >
       <div className="mx-auto max-w-360 px-20 py-4 flex justify-between items-center">
         <Link href="/">
           <Button variant="ghost" size="icon">
